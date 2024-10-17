b648171f74af25296af61855da4ac0255c08c7d6
DevRex-0201 <dev.rex.0201@gmail.com>
2024-10-17 23:53:16 +0900

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ const PreLoadingScreen = ({ progress }: { progress: number }) => {
   return (
     <div className="flex w-full min-h-screen flex-col items-center bg-[#1EB17B] pb-14 gap-6">
       <div className="w-full bg-[#22CA8C] bg-[url('/images/preloading_background.png'),url('/images/bubbles.png')] bg-[length:100%_100%,cover] bg-no-repeat pb-10">
-        <div className="w-full flex justify-center pt-10">
+        <div className="w-full flex justify-center pt-6">
           <Image
             src={"/images/cookie_jar.png"}
             alt="Cookie Logo"
